14c5c9f73174fb3117a422b39db8e9a9980b9567
laurenmarsh <laurenmdb@gmail.com>
2023-12-08 09:09:35 -0500

    add intro video

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a78b73c2aadef7dbbdf9b9f8f9916d8efcd8ced0b26daa9524bcb8065b7723f2
-size 15261
+oid sha256:9fbcb45996accbe65336ff8108b09db9f04cc7cffda8c15b52870d9f6d6fa865
+size 16326
